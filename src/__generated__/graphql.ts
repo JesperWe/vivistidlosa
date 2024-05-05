b6d2c1aa9b91f88e9a4bbe730198d136d0628bb5
jesperwe <jesper@journeyman.se>
2024-05-05 20:03:52 +0200

    Carousel

---
+++ src/__generated__/graphql.ts
@@ -0,0 +1,414 @@
+/* eslint-disable */
+import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/core';
+export type Maybe<T> = T | null;
+export type InputMaybe<T> = Maybe<T>;
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+export type MakeEmpty<T extends { [key: string]: unknown }, K extends keyof T> = { [_ in K]?: never };
+export type Incremental<T> = T | { [P in keyof T]?: P extends ' $fragmentName' | '__typename' ? T[P] : never };
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+  ID: { input: string; output: string; }
+  String: { input: string; output: string; }
+  Boolean: { input: boolean; output: boolean; }
+  Int: { input: number; output: number; }
+  Float: { input: number; output: number; }
+  /** ISO8601 Date values */
+  Date: { input: any; output: any; }
+  /** BigInt value */
+  GraphQLBigInt: { input: any; output: any; }
+  /** A Float or a String */
+  GraphQLStringOrFloat: { input: any; output: any; }
+  /** The `JSON` scalar type represents JSON values as specified by [ECMA-404](http://www.ecma-international.org/publications/files/ECMA-ST/ECMA-404.pdf). */
+  JSON: { input: any; output: any; }
+};
+
+export enum EventEnum {
+  Create = 'create',
+  Delete = 'delete',
+  Update = 'update'
+}
+
+export type Query = {
+  __typename?: 'Query';
+  bouquet: Array<Bouquet>;
+  bouquet_aggregated: Array<Bouquet_Aggregated>;
+  bouquet_by_id?: Maybe<Bouquet>;
+  bouquet_by_version?: Maybe<Version_Bouquet>;
+};
+
+
+export type QueryBouquetArgs = {
+  filter?: InputMaybe<Bouquet_Filter>;
+  limit?: InputMaybe<Scalars['Int']['input']>;
+  offset?: InputMaybe<Scalars['Int']['input']>;
+  page?: InputMaybe<Scalars['Int']['input']>;
+  search?: InputMaybe<Scalars['String']['input']>;
+  sort?: InputMaybe<Array<InputMaybe<Scalars['String']['input']>>>;
+};
+
+
+export type QueryBouquet_AggregatedArgs = {
+  filter?: InputMaybe<Bouquet_Filter>;
+  groupBy?: InputMaybe<Array<InputMaybe<Scalars['String']['input']>>>;
+  limit?: InputMaybe<Scalars['Int']['input']>;
+  offset?: InputMaybe<Scalars['Int']['input']>;
+  page?: InputMaybe<Scalars['Int']['input']>;
+  search?: InputMaybe<Scalars['String']['input']>;
+  sort?: InputMaybe<Array<InputMaybe<Scalars['String']['input']>>>;
+};
+
+
+export type QueryBouquet_By_IdArgs = {
+  id: Scalars['ID']['input'];
+  version?: InputMaybe<Scalars['String']['input']>;
+};
+
+
+export type QueryBouquet_By_VersionArgs = {
+  id: Scalars['ID']['input'];
+  version: Scalars['String']['input'];
+};
+
+export type Subscription = {
+  __typename?: 'Subscription';
+  bouquet_mutated?: Maybe<Bouquet_Mutated>;
+  directus_files_mutated?: Maybe<Directus_Files_Mutated>;
+  directus_folders_mutated?: Maybe<Directus_Folders_Mutated>;
+};
+
+
+export type SubscriptionBouquet_MutatedArgs = {
+  event?: InputMaybe<EventEnum>;
+};
+
+
+export type SubscriptionDirectus_Files_MutatedArgs = {
+  event?: InputMaybe<EventEnum>;
+};
+
+
+export type SubscriptionDirectus_Folders_MutatedArgs = {
+  event?: InputMaybe<EventEnum>;
+};
+
+export type Big_Int_Filter_Operators = {
+  _between?: InputMaybe<Array<InputMaybe<Scalars['GraphQLBigInt']['input']>>>;
+  _eq?: InputMaybe<Scalars['GraphQLBigInt']['input']>;
+  _gt?: InputMaybe<Scalars['GraphQLBigInt']['input']>;
+  _gte?: InputMaybe<Scalars['GraphQLBigInt']['input']>;
+  _in?: InputMaybe<Array<InputMaybe<Scalars['GraphQLBigInt']['input']>>>;
+  _lt?: InputMaybe<Scalars['GraphQLBigInt']['input']>;
+  _lte?: InputMaybe<Scalars['GraphQLBigInt']['input']>;
+  _nbetween?: InputMaybe<Array<InputMaybe<Scalars['GraphQLBigInt']['input']>>>;
+  _neq?: InputMaybe<Scalars['GraphQLBigInt']['input']>;
+  _nin?: InputMaybe<Array<InputMaybe<Scalars['GraphQLBigInt']['input']>>>;
+  _nnull?: InputMaybe<Scalars['Boolean']['input']>;
+  _null?: InputMaybe<Scalars['Boolean']['input']>;
+};
+
+export type Bouquet = {
+  __typename?: 'bouquet';
+  date_created?: Maybe<Scalars['Date']['output']>;
+  date_created_func?: Maybe<Datetime_Functions>;
+  date_updated?: Maybe<Scalars['Date']['output']>;
+  date_updated_func?: Maybe<Datetime_Functions>;
+  hero_image?: Maybe<Directus_Files>;
+  id: Scalars['ID']['output'];
+  name?: Maybe<Scalars['String']['output']>;
+  price?: Maybe<Scalars['Int']['output']>;
+  sort?: Maybe<Scalars['Int']['output']>;
+  status?: Maybe<Scalars['String']['output']>;
+  user_created?: Maybe<Scalars['String']['output']>;
+  user_updated?: Maybe<Scalars['String']['output']>;
+};
+
+
+export type BouquetHero_ImageArgs = {
+  filter?: InputMaybe<Directus_Files_Filter>;
+  limit?: InputMaybe<Scalars['Int']['input']>;
+  offset?: InputMaybe<Scalars['Int']['input']>;
+  page?: InputMaybe<Scalars['Int']['input']>;
+  search?: InputMaybe<Scalars['String']['input']>;
+  sort?: InputMaybe<Array<InputMaybe<Scalars['String']['input']>>>;
+};
+
+export type Bouquet_Aggregated = {
+  __typename?: 'bouquet_aggregated';
+  avg?: Maybe<Bouquet_Aggregated_Fields>;
+  avgDistinct?: Maybe<Bouquet_Aggregated_Fields>;
+  count?: Maybe<Bouquet_Aggregated_Count>;
+  countAll?: Maybe<Scalars['Int']['output']>;
+  countDistinct?: Maybe<Bouquet_Aggregated_Count>;
+  group?: Maybe<Scalars['JSON']['output']>;
+  max?: Maybe<Bouquet_Aggregated_Fields>;
+  min?: Maybe<Bouquet_Aggregated_Fields>;
+  sum?: Maybe<Bouquet_Aggregated_Fields>;
+  sumDistinct?: Maybe<Bouquet_Aggregated_Fields>;
+};
+
+export type Bouquet_Aggregated_Count = {
+  __typename?: 'bouquet_aggregated_count';
+  date_created?: Maybe<Scalars['Int']['output']>;
+  date_updated?: Maybe<Scalars['Int']['output']>;
+  hero_image?: Maybe<Scalars['Int']['output']>;
+  id?: Maybe<Scalars['Int']['output']>;
+  name?: Maybe<Scalars['Int']['output']>;
+  price?: Maybe<Scalars['Int']['output']>;
+  sort?: Maybe<Scalars['Int']['output']>;
+  status?: Maybe<Scalars['Int']['output']>;
+  user_created?: Maybe<Scalars['Int']['output']>;
+  user_updated?: Maybe<Scalars['Int']['output']>;
+};
+
+export type Bouquet_Aggregated_Fields = {
+  __typename?: 'bouquet_aggregated_fields';
+  id?: Maybe<Scalars['Float']['output']>;
+  price?: Maybe<Scalars['Float']['output']>;
+  sort?: Maybe<Scalars['Float']['output']>;
+};
+
+export type Bouquet_Filter = {
+  _and?: InputMaybe<Array<InputMaybe<Bouquet_Filter>>>;
+  _or?: InputMaybe<Array<InputMaybe<Bouquet_Filter>>>;
+  date_created?: InputMaybe<Date_Filter_Operators>;
+  date_created_func?: InputMaybe<Datetime_Function_Filter_Operators>;
+  date_updated?: InputMaybe<Date_Filter_Operators>;
+  date_updated_func?: InputMaybe<Datetime_Function_Filter_Operators>;
+  hero_image?: InputMaybe<Directus_Files_Filter>;
+  id?: InputMaybe<Number_Filter_Operators>;
+  name?: InputMaybe<String_Filter_Operators>;
+  price?: InputMaybe<Number_Filter_Operators>;
+  sort?: InputMaybe<Number_Filter_Operators>;
+  status?: InputMaybe<String_Filter_Operators>;
+  user_created?: InputMaybe<String_Filter_Operators>;
+  user_updated?: InputMaybe<String_Filter_Operators>;
+};
+
+export type Bouquet_Mutated = {
+  __typename?: 'bouquet_mutated';
+  data?: Maybe<Bouquet>;
+  event?: Maybe<EventEnum>;
+  key: Scalars['ID']['output'];
+};
+
+export type Count_Function_Filter_Operators = {
+  count?: InputMaybe<Number_Filter_Operators>;
+};
+
+export type Count_Functions = {
+  __typename?: 'count_functions';
+  count?: Maybe<Scalars['Int']['output']>;
+};
+
+export type Date_Filter_Operators = {
+  _between?: InputMaybe<Array<InputMaybe<Scalars['GraphQLStringOrFloat']['input']>>>;
+  _eq?: InputMaybe<Scalars['String']['input']>;
+  _gt?: InputMaybe<Scalars['String']['input']>;
+  _gte?: InputMaybe<Scalars['String']['input']>;
+  _in?: InputMaybe<Array<InputMaybe<Scalars['String']['input']>>>;
+  _lt?: InputMaybe<Scalars['String']['input']>;
+  _lte?: InputMaybe<Scalars['String']['input']>;
+  _nbetween?: InputMaybe<Array<InputMaybe<Scalars['GraphQLStringOrFloat']['input']>>>;
+  _neq?: InputMaybe<Scalars['String']['input']>;
+  _nin?: InputMaybe<Array<InputMaybe<Scalars['String']['input']>>>;
+  _nnull?: InputMaybe<Scalars['Boolean']['input']>;
+  _null?: InputMaybe<Scalars['Boolean']['input']>;
+};
+
+export type Datetime_Function_Filter_Operators = {
+  day?: InputMaybe<Number_Filter_Operators>;
+  hour?: InputMaybe<Number_Filter_Operators>;
+  minute?: InputMaybe<Number_Filter_Operators>;
+  month?: InputMaybe<Number_Filter_Operators>;
+  second?: InputMaybe<Number_Filter_Operators>;
+  week?: InputMaybe<Number_Filter_Operators>;
+  weekday?: InputMaybe<Number_Filter_Operators>;
+  year?: InputMaybe<Number_Filter_Operators>;
+};
+
+export type Datetime_Functions = {
+  __typename?: 'datetime_functions';
+  day?: Maybe<Scalars['Int']['output']>;
+  hour?: Maybe<Scalars['Int']['output']>;
+  minute?: Maybe<Scalars['Int']['output']>;
+  month?: Maybe<Scalars['Int']['output']>;
+  second?: Maybe<Scalars['Int']['output']>;
+  week?: Maybe<Scalars['Int']['output']>;
+  weekday?: Maybe<Scalars['Int']['output']>;
+  year?: Maybe<Scalars['Int']['output']>;
+};
+
+export type Directus_Files = {
+  __typename?: 'directus_files';
+  charset?: Maybe<Scalars['String']['output']>;
+  description?: Maybe<Scalars['String']['output']>;
+  duration?: Maybe<Scalars['Int']['output']>;
+  embed?: Maybe<Scalars['String']['output']>;
+  filename_disk?: Maybe<Scalars['String']['output']>;
+  filename_download: Scalars['String']['output'];
+  filesize?: Maybe<Scalars['GraphQLBigInt']['output']>;
+  focal_point_x?: Maybe<Scalars['Int']['output']>;
+  focal_point_y?: Maybe<Scalars['Int']['output']>;
+  folder?: Maybe<Directus_Folders>;
+  height?: Maybe<Scalars['Int']['output']>;
+  id: Scalars['ID']['output'];
+  location?: Maybe<Scalars['String']['output']>;
+  metadata?: Maybe<Scalars['JSON']['output']>;
+  metadata_func?: Maybe<Count_Functions>;
+  modified_by?: Maybe<Scalars['String']['output']>;
+  modified_on?: Maybe<Scalars['Date']['output']>;
+  modified_on_func?: Maybe<Datetime_Functions>;
+  storage: Scalars['String']['output'];
+  tags?: Maybe<Scalars['JSON']['output']>;
+  tags_func?: Maybe<Count_Functions>;
+  title?: Maybe<Scalars['String']['output']>;
+  type?: Maybe<Scalars['String']['output']>;
+  uploaded_by?: Maybe<Scalars['String']['output']>;
+  uploaded_on?: Maybe<Scalars['Date']['output']>;
+  uploaded_on_func?: Maybe<Datetime_Functions>;
+  width?: Maybe<Scalars['Int']['output']>;
+};
+
+
+export type Directus_FilesFolderArgs = {
+  filter?: InputMaybe<Directus_Folders_Filter>;
+  limit?: InputMaybe<Scalars['Int']['input']>;
+  offset?: InputMaybe<Scalars['Int']['input']>;
+  page?: InputMaybe<Scalars['Int']['input']>;
+  search?: InputMaybe<Scalars['String']['input']>;
+  sort?: InputMaybe<Array<InputMaybe<Scalars['String']['input']>>>;
+};
+
+export type Directus_Files_Filter = {
+  _and?: InputMaybe<Array<InputMaybe<Directus_Files_Filter>>>;
+  _or?: InputMaybe<Array<InputMaybe<Directus_Files_Filter>>>;
+  charset?: InputMaybe<String_Filter_Operators>;
+  description?: InputMaybe<String_Filter_Operators>;
+  duration?: InputMaybe<Number_Filter_Operators>;
+  embed?: InputMaybe<String_Filter_Operators>;
+  filename_disk?: InputMaybe<String_Filter_Operators>;
+  filename_download?: InputMaybe<String_Filter_Operators>;
+  filesize?: InputMaybe<Big_Int_Filter_Operators>;
+  focal_point_x?: InputMaybe<Number_Filter_Operators>;
+  focal_point_y?: InputMaybe<Number_Filter_Operators>;
+  folder?: InputMaybe<Directus_Folders_Filter>;
+  height?: InputMaybe<Number_Filter_Operators>;
+  id?: InputMaybe<String_Filter_Operators>;
+  location?: InputMaybe<String_Filter_Operators>;
+  metadata?: InputMaybe<String_Filter_Operators>;
+  metadata_func?: InputMaybe<Count_Function_Filter_Operators>;
+  modified_by?: InputMaybe<String_Filter_Operators>;
+  modified_on?: InputMaybe<Date_Filter_Operators>;
+  modified_on_func?: InputMaybe<Datetime_Function_Filter_Operators>;
+  storage?: InputMaybe<String_Filter_Operators>;
+  tags?: InputMaybe<String_Filter_Operators>;
+  tags_func?: InputMaybe<Count_Function_Filter_Operators>;
+  title?: InputMaybe<String_Filter_Operators>;
+  type?: InputMaybe<String_Filter_Operators>;
+  uploaded_by?: InputMaybe<String_Filter_Operators>;
+  uploaded_on?: InputMaybe<Date_Filter_Operators>;
+  uploaded_on_func?: InputMaybe<Datetime_Function_Filter_Operators>;
+  width?: InputMaybe<Number_Filter_Operators>;
+};
+
+export type Directus_Files_Mutated = {
+  __typename?: 'directus_files_mutated';
+  data?: Maybe<Directus_Files>;
+  event?: Maybe<EventEnum>;
+  key: Scalars['ID']['output'];
+};
+
+export type Directus_Folders = {
+  __typename?: 'directus_folders';
+  id: Scalars['ID']['output'];
+  name: Scalars['String']['output'];
+  parent?: Maybe<Directus_Folders>;
+};
+
+
+export type Directus_FoldersParentArgs = {
+  filter?: InputMaybe<Directus_Folders_Filter>;
+  limit?: InputMaybe<Scalars['Int']['input']>;
+  offset?: InputMaybe<Scalars['Int']['input']>;
+  page?: InputMaybe<Scalars['Int']['input']>;
+  search?: InputMaybe<Scalars['String']['input']>;
+  sort?: InputMaybe<Array<InputMaybe<Scalars['String']['input']>>>;
+};
+
+export type Directus_Folders_Filter = {
+  _and?: InputMaybe<Array<InputMaybe<Directus_Folders_Filter>>>;
+  _or?: InputMaybe<Array<InputMaybe<Directus_Folders_Filter>>>;
+  id?: InputMaybe<String_Filter_Operators>;
+  name?: InputMaybe<String_Filter_Operators>;
+  parent?: InputMaybe<Directus_Folders_Filter>;
+};
+
+export type Directus_Folders_Mutated = {
+  __typename?: 'directus_folders_mutated';
+  data?: Maybe<Directus_Folders>;
+  event?: Maybe<EventEnum>;
+  key: Scalars['ID']['output'];
+};
+
+export type Number_Filter_Operators = {
+  _between?: InputMaybe<Array<InputMaybe<Scalars['GraphQLStringOrFloat']['input']>>>;
+  _eq?: InputMaybe<Scalars['GraphQLStringOrFloat']['input']>;
+  _gt?: InputMaybe<Scalars['GraphQLStringOrFloat']['input']>;
+  _gte?: InputMaybe<Scalars['GraphQLStringOrFloat']['input']>;
+  _in?: InputMaybe<Array<InputMaybe<Scalars['GraphQLStringOrFloat']['input']>>>;
+  _lt?: InputMaybe<Scalars['GraphQLStringOrFloat']['input']>;
+  _lte?: InputMaybe<Scalars['GraphQLStringOrFloat']['input']>;
+  _nbetween?: InputMaybe<Array<InputMaybe<Scalars['GraphQLStringOrFloat']['input']>>>;
+  _neq?: InputMaybe<Scalars['GraphQLStringOrFloat']['input']>;
+  _nin?: InputMaybe<Array<InputMaybe<Scalars['GraphQLStringOrFloat']['input']>>>;
+  _nnull?: InputMaybe<Scalars['Boolean']['input']>;
+  _null?: InputMaybe<Scalars['Boolean']['input']>;
+};
+
+export type String_Filter_Operators = {
+  _contains?: InputMaybe<Scalars['String']['input']>;
+  _empty?: InputMaybe<Scalars['Boolean']['input']>;
+  _ends_with?: InputMaybe<Scalars['String']['input']>;
+  _eq?: InputMaybe<Scalars['String']['input']>;
+  _icontains?: InputMaybe<Scalars['String']['input']>;
+  _iends_with?: InputMaybe<Scalars['String']['input']>;
+  _in?: InputMaybe<Array<InputMaybe<Scalars['String']['input']>>>;
+  _istarts_with?: InputMaybe<Scalars['String']['input']>;
+  _ncontains?: InputMaybe<Scalars['String']['input']>;
+  _nempty?: InputMaybe<Scalars['Boolean']['input']>;
+  _nends_with?: InputMaybe<Scalars['String']['input']>;
+  _neq?: InputMaybe<Scalars['String']['input']>;
+  _niends_with?: InputMaybe<Scalars['String']['input']>;
+  _nin?: InputMaybe<Array<InputMaybe<Scalars['String']['input']>>>;
+  _nistarts_with?: InputMaybe<Scalars['String']['input']>;
+  _nnull?: InputMaybe<Scalars['Boolean']['input']>;
+  _nstarts_with?: InputMaybe<Scalars['String']['input']>;
+  _null?: InputMaybe<Scalars['Boolean']['input']>;
+  _starts_with?: InputMaybe<Scalars['String']['input']>;
+};
+
+export type Version_Bouquet = {
+  __typename?: 'version_bouquet';
+  date_created?: Maybe<Scalars['Date']['output']>;
+  date_created_func?: Maybe<Datetime_Functions>;
+  date_updated?: Maybe<Scalars['Date']['output']>;
+  date_updated_func?: Maybe<Datetime_Functions>;
+  hero_image?: Maybe<Scalars['JSON']['output']>;
+  id: Scalars['ID']['output'];
+  name?: Maybe<Scalars['String']['output']>;
+  price?: Maybe<Scalars['Int']['output']>;
+  sort?: Maybe<Scalars['Int']['output']>;
+  status?: Maybe<Scalars['String']['output']>;
+  user_created?: Maybe<Scalars['String']['output']>;
+  user_updated?: Maybe<Scalars['String']['output']>;
+};
+
+export type BouquetsQueryVariables = Exact<{ [key: string]: never; }>;
+
+
+export type BouquetsQuery = { __typename?: 'Query', bouquet: Array<{ __typename?: 'bouquet', id: string, name?: string | null, price?: number | null, hero_image?: { __typename?: 'directus_files', filename_disk?: string | null, filename_download: string, title?: string | null, focal_point_x?: number | null, focal_point_y?: number | null } | null }> };
+
+
+export const BouquetsDocument = {"kind":"Document","definitions":[{"kind":"OperationDefinition","operation":"query","name":{"kind":"Name","value":"Bouquets"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"bouquet"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"id"}},{"kind":"Field","name":{"kind":"Name","value":"name"}},{"kind":"Field","name":{"kind":"Name","value":"price"}},{"kind":"Field","name":{"kind":"Name","value":"hero_image"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"filename_disk"}},{"kind":"Field","name":{"kind":"Name","value":"filename_download"}},{"kind":"Field","name":{"kind":"Name","value":"title"}},{"kind":"Field","name":{"kind":"Name","value":"focal_point_x"}},{"kind":"Field","name":{"kind":"Name","value":"focal_point_y"}}]}}]}}]}}]} as unknown as DocumentNode<BouquetsQuery, BouquetsQueryVariables>;
